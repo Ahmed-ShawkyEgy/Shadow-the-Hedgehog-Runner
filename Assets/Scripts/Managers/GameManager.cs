31a098a92c3aeba7aaf5430bb599c9e63c6233eb
unknown <ahmed.shawkyegy@gmail.com>
2019-10-08 18:17:47 +0200

    implement timer feature

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,18 +12,32 @@ public class GameManager : MonoBehaviour
     private GameObject ball;
 
     private float distanceTravelled , lastPosition;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
         distanceTravelled = 0;
+        timer = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateDistanceTravelled();
+        UpdateTimer();
+    }
+
+    private void UpdateDistanceTravelled()
+    {
         distanceTravelled += ball.transform.position.z - lastPosition;
         lastPosition = ball.transform.position.z;
-        distanceDisplay.text = (distanceTravelled/10).ToString("F1");
+        distanceDisplay.text = (distanceTravelled / 10).ToString("F1");
+    }
+
+    private void UpdateTimer()
+    {
+        timer -= Time.deltaTime;
+        timeDisplay.text = timer.ToString("F0");
     }
 }
