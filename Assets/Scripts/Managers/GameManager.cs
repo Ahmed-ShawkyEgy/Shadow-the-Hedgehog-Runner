c7074fa2cd69cc55ce45acd2d4158aa70e1b78d9
unknown <ahmed.shawkyegy@gmail.com>
2019-10-08 18:14:16 +0200

    implement distance travelled feature

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMeshProUGUI timeDisplay, distanceDisplay;
+    [SerializeField]
+    private GameObject ball;
+
+    private float distanceTravelled , lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceTravelled = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += ball.transform.position.z - lastPosition;
+        lastPosition = ball.transform.position.z;
+        distanceDisplay.text = (distanceTravelled/10).ToString("F1");
+    }
+}
