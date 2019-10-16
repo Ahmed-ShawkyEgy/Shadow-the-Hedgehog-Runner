fd9dad316f9ab2203e4a31822b63d79e444c4cd2
unknown <ahmed.shawkyegy@gmail.com>
2019-10-16 13:57:10 +0200

    add credits and how to play menues

---
+++ Assets/Scripts/Controllers/CreditsMenue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenue : MonoBehaviour
+{
+
+    public void OnCreditsEnd()
+    {
+        MainMenueManager.Instance.DisplayOptionsMenue();
+    }
+}
